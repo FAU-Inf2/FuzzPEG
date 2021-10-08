5e1e440cdcfeb8977d7f129d15b0c9a16bf95214
Patrick Kreutzer <patrick.kreutzer@fau.de>
2021-10-08 17:06:00 +0200

    Add 'RandomTokenGenerator'.

---
+++ src/main/java/i2/act/fuzzer/RandomTokenGenerator.java
@@ -0,0 +1,147 @@
+package i2.act.fuzzer;
+
+import i2.act.grammargraph.GrammarGraph;
+import i2.act.packrat.Token;
+import i2.act.packrat.nfa.CharacterSet;
+import i2.act.packrat.nfa.NFA;
+import i2.act.packrat.nfa.NFAState;
+import i2.act.packrat.nfa.Transition;
+import i2.act.peg.ast.CharacterRange;
+import i2.act.peg.ast.Group;
+import i2.act.peg.ast.LexerProduction;
+import i2.act.peg.ast.Range;
+import i2.act.peg.ast.RegularExpression;
+import i2.act.peg.ast.SingleCharacter;
+import i2.act.peg.symbols.LexerSymbol;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public final class RandomTokenGenerator {
+
+  private final Map<LexerSymbol, NFA> automata;
+  private final Random rng;
+
+  public RandomTokenGenerator(final GrammarGraph grammarGraph, final Random rng) {
+    this.automata = constructAutomata(grammarGraph);
+    this.rng = rng;
+  }
+
+  private final Map<LexerSymbol, NFA> constructAutomata(final GrammarGraph grammarGraph) {
+    final Map<LexerSymbol, NFA> automata = new HashMap<>();
+
+    for (final LexerSymbol lexerSymbol : grammarGraph.gatherLexerSymbols()) {
+      if (lexerSymbol == lexerSymbol.EOF) {
+        continue;
+      }
+
+      final LexerProduction lexerProduction = lexerSymbol.getProduction();
+      assert (lexerProduction != null);
+
+      final RegularExpression regularExpression = lexerProduction.getRegularExpression();
+      final NFA automaton = NFA.fromRegularExpression(regularExpression);
+
+      automata.put(lexerSymbol, automaton);
+    }
+
+    return automata;
+  }
+
+  public final Token createRandomToken(final LexerSymbol lexerSymbol) {
+    if (lexerSymbol == LexerSymbol.EOF) {
+      return new Token(lexerSymbol, null);
+    }
+
+    final NFA nfa = this.automata.get(lexerSymbol);
+    assert (nfa != null);
+
+    final String tokenValue;
+    {
+      if (nfa.hasLiteralString()) {
+        tokenValue = nfa.getLiteralString();
+      } else {
+        final StringBuilder builder = new StringBuilder();
+        createRandomString(nfa, builder);
+        tokenValue = builder.toString();
+      }
+    }
+
+    return new Token(lexerSymbol, tokenValue);
+  }
+
+  private final void createRandomString(final NFA nfa, final StringBuilder builder) {
+    final Set<NFAState> acceptingStates = nfa.getAcceptingStates();
+    NFAState currentState = nfa.getStartState();
+
+    while (true) {
+      final List<Transition> transitions = currentState.getTransitions();
+
+      if (acceptingStates.contains(currentState)) {
+        if (transitions.isEmpty() || this.rng.nextBoolean()) {
+          return;
+        }
+      }
+
+      assert (!transitions.isEmpty());
+      final Transition transition = transitions.get(this.rng.nextInt(transitions.size()));
+
+      if (!transition.isEpsilonTransition()) {
+        builder.append(randomCharacter(transition.getCharacters()));
+      }
+
+      currentState = transition.getTo();
+    }
+  }
+
+  private final char randomCharacter(final CharacterSet characterSet) {
+    if (characterSet instanceof CharacterSet.SingleCharacter) {
+      return ((CharacterSet.SingleCharacter) characterSet).getCharacter();
+    } else {
+      assert (characterSet instanceof CharacterSet.CharacterGroup);
+
+      final CharacterSet.CharacterGroup characterGroup = (CharacterSet.CharacterGroup) characterSet;
+      final Group group = characterGroup.getGroup();
+
+      if (group.isInverted()) {
+        // iterate over printable (ASCII) characters and check if there is a valid one
+        for (char character = ' '; character <= '~'; ++character) {
+          if (characterGroup.matches(character)) {
+            return character;
+          }
+        }
+
+        throw new RuntimeException("did not find a valid character");
+      } else {
+        final List<Range> ranges = group.getRanges();
+        final Range randomRange = ranges.get(this.rng.nextInt(ranges.size()));
+
+        final char lower = getLower(randomRange);
+        final char upper = getUpper(randomRange);
+
+        return (char) (this.rng.nextInt(upper - lower + 1) + lower);
+      }
+    }
+  }
+
+  private final char getLower(final Range range) {
+    if (range instanceof SingleCharacter) {
+      return ((SingleCharacter) range).getValue();
+    } else {
+      assert (range instanceof CharacterRange);
+      return ((CharacterRange) range).getLowerCharacter().getValue();
+    }
+  }
+
+  private final char getUpper(final Range range) {
+    if (range instanceof SingleCharacter) {
+      return ((SingleCharacter) range).getValue();
+    } else {
+      assert (range instanceof CharacterRange);
+      return ((CharacterRange) range).getUpperCharacter().getValue();
+    }
+  }
+
+}
