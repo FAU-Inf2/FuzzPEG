e88c0a16306260ccf1158b1e92e007a55eb23a31
Patrick Kreutzer <patrick.kreutzer@fau.de>
2021-10-21 11:36:27 +0200

    Fix computation of minimum required 'maxHeight'.

--- src/main/java/i2/act/grammargraph/properties/MinMaxHeightComputation.java
+++ src/main/java/i2/act/grammargraph/properties/MinMaxHeightComputation.java
@@ -2,40 +2,25 @@ package i2.act.grammargraph.properties;
 
 import i2.act.grammargraph.GrammarGraph;
 import i2.act.grammargraph.GrammarGraphNode;
-import i2.act.grammargraph.GrammarGraphNode.Choice;
 
 import java.util.Map;
 
 public final class MinMaxHeightComputation {
 
   public static final int computeMinMaxHeight(final GrammarGraph grammarGraph) {
-    final Map<GrammarGraphNode<?, ?>, Integer> minHeights =
-        MinHeightComputation.computeMinHeights(
-            grammarGraph, MinHeightComputation.Mode.ALL_ALTERNATIVES);
+    final Map<GrammarGraphNode<?,?>, Map<GrammarGraphNode<?,?>, Integer>> reachableNodes =
+        ReachableNodesComputation.computeReachableNodes(grammarGraph);
 
-    final Map<GrammarGraphNode<?, ?>, Integer> minDepths =
-        MinDepthComputation.computeMinDepths(grammarGraph);
+    final GrammarGraphNode<?,?> rootNode = grammarGraph.getRootNode();
+    assert (reachableNodes.containsKey(rootNode));
 
-    final Map<GrammarGraphNode<?, ?>, Boolean> reachable =
-        ReachableComputation.computeReachable(grammarGraph, false);
+    final Map<GrammarGraphNode<?,?>, Integer> reachableFromRootNode = reachableNodes.get(rootNode);
 
     int minMaxHeight = 0;
 
-    for (final Map.Entry<GrammarGraphNode<?,?>, Boolean> entry : reachable.entrySet()) {
-      final GrammarGraphNode<?,?> node = entry.getKey();
-      final Boolean isReachable = entry.getValue();
-
-      if ((!isReachable) || !(node instanceof Choice)) {
-        continue;
-      }
-
-      assert (minHeights.containsKey(node));
-      assert (minDepths.containsKey(node));
-
-      final int minMaxHeightNode = minHeights.get(node) + minDepths.get(node);
-
-      if (minMaxHeightNode > minMaxHeight) {
-        minMaxHeight = minMaxHeightNode;
+    for (final Integer requiredHeight : reachableFromRootNode.values()) {
+      if (requiredHeight > minMaxHeight) {
+        minMaxHeight = requiredHeight;
       }
     }
 
